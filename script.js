96a8490d69de763d0fd9596b41de39f7470af5a3
Cristhian de Souza <111239606+crisouzath@users.noreply.github.com>
2022-09-16 20:50:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,188 @@
+
+const  musica1={
+    nome:"Try",
+    singer:"P!nk",
+    arquivo:"musicas/P!nk - Try.mp3",
+    imagem:"url(img/try.jpg)",
+    tempo: "4:10",
+    url: "try.html",
+    letra:"Ever wonder 'bout what he's doin'?<br>How it all turned to lies?<br>Sometimes I think that it's better<br>To never ask why<br>Where there is desire, there is gonna be a flame<br>Where there is a flame, someone's bound to get burned<br>But just because it burns doesn't mean you're gonna die<br> You've gotta get up and try, try, try<br> Gotta get up and try, try, try<br> Gotta get up and try, try, try<br> Funny how the heart can be deceiving<br> More than just a couple times<br> Why do we fall in love so easy<br> Even when it's not right?<br> Where there is desire, there is gonna be a flame<br> Where there is a flame, someone's bound to get burned<br> But just because it burns doesn't mean you're gonna die<br> You've gotta get up and try, try, try<br> Gotta get up and try, try, try<br> You've gotta get up and try, try, try<br> Ever worry that it might be ruined<br> And does it make you wanna cry?<br> When you're out there doing what you're doing<br> Are you just getting by?<br> Tell me, are you just getting by, by, by?<br> Where there is desire, there is gonna be a flame<br> Where there is a flame, someone's bound to get burned<br> But just because it burns doesn't mean you're gonna die<br> You've gotta get up and try, try, try<br> Gotta get up and try, try, try<br> You've gotta get up and try, try, try<br> Gotta get up and try, try, try<br> Gotta get up and try, try, try<br> You've gotta get up and try, try, try<br> Gotta get up and try, try, try<br> You've gotta get up and try, try, try<br> Gotta get up and try, try, try<br>"
+}
+const musica3={
+    nome:"Ten Million Voices",
+    singer:"Shaman's Harvest",
+    arquivo:"musicas/Ten Million Voices.mp3",
+    imagem:"url(img/ten-million-voices.jpg)",
+    tempo: "3:58",
+    url: "ten-million.html",
+letra:"Are the whispers turning your mind?<br>They're the wolves nipping at your heels<br>Trying to drag you down when the light is gone<br>And now winter is coming, she preys on us all<br>Watch how they're mocking, ready to fall<br>And our pulses are pounding to the sound of the drum<br>When it feels like your heart has been bled<br>And there's no more left to be said<br>Trying not to fall<br>There are ten million souls who are feeling like you<br>And ten million more who are finding the truth<br>Then it feels like you're right where you belong<br>When ten million voices are one<br>If you've finally had enough<br>Turn your fist to the sun<br>It's time to raise your voices and become one<br>And now winter is coming, she preys on us all<br>Watch how they're mocking, ready to fall<br>And our pulses are pounding to the sound of the drum<br>When it feels like your heart has been bled<br>And there's no more left to be said<br>Trying not to fall<br>There are ten million souls who are feeling like you<br>And ten million more who are finding the truth<br>Then it feels like you're right where you belong<br>When ten million voices are one<br>Lay down this crutch and bleed for us all<br>Will you have the stones to weather the storm?<br>Open your eyes<br>'Cause you got to make up your mind<br>When it feels like your heart has been bled<br>And there's no more left to be said<br>Trying not to fall<br>When it feels like your heart has been bled<br>And there's no more left to be said<br>Trying not to fall<br>There are ten million souls who are feeling like you<br>And ten million more who are finding the truth<br>Then it feels like you're right where you belong<br>When ten million voices are one<br>When ten million voices are one<br>"
+}
+
+const musica2={
+    nome:"Don't Think Twice",
+    singer:"Hikaru Utada",
+    arquivo:"musicas/Don't Think Twice.mp3",
+    imagem:"url(img/dont-think-twice.jpeg)",
+    tempo: "4:33",
+    url: "dont-think-twice.html",
+letra:"How did I live in a kingdom of thieves<br>And people who say things they don't really mean, really mean<br>You're only everything I ever dreamed<br>Ever dreamed of, ever dreamed of<br>You must be kidding me, did you really think<br>I could say no<br>I want you for a lifetime<br>So if you're gonna think twice, baby<br>I don't wanna know, baby, I don't wanna know<br>Everything is just right<br>But if you're gonna think twice, baby<br>I don't wanna know, baby, I don't wanna know<br>I really don't get what everyone else believes<br>So why do I say things I don't really mean, really mean<br>I'm only crying 'cause I never dreamed<br>It'd take this long, it'd take this long<br>I want you for a lifetime<br>So if you're gonna think twice, baby<br>I don't wanna know, baby, I don't wanna know<br>Everything is just right<br>But if you're gonna think twice, baby<br>I don't wanna know, baby, I don't wanna know<br>Don't think twice, don't think twice<br>Don't think twice, baby, don't think twice<br>If you want to take it to an even higher level<br>All you gotta do is say the word, you know I'll follow<br>If you wanna take it to an even higher level<brI don't, I don't bite<brIf you want to make it happen, nothing's impossible<brAll you gotta do is say the word, the walls will crumble<brIf you want to make it happen, nothing's impossible<brI want you for a lifetime<brSo if you're gonna think twice, baby<brI don't wanna know, baby, I don't wanna know<brEverything is just right<brBut if you're gonna think twice, baby<brI don't wanna know, baby, I don't wanna know<brKiss me once, kiss me twice, kiss me three times<br>Cross the line<br>Don't think twice, don't think twice<br>Don't think twice, baby, don't think twice<br>Kiss me once, kiss me twice, kiss me three times<br>Cross the line<br>Kiss me once, kiss me twice, kiss me three times<br>Be mine<br>Don't think twice, don't think twice<br>Don't think twice, baby, don't think twice<br>"
+}
+
+const musica4={
+    nome:"D-Tecnolife",
+    singer:"UVERworld",
+    arquivo:"musicas/UVERworld - D-TecnoLife.mp3",
+    imagem:"url(img/dtecnolife.jpg)",
+    tempo: "3:49",
+    url: "d-tecnolife.html",
+letra:"Ienai itami kanashimi de kizu tsuita kimi yo<br>Kesenai kako mo seoi atte ikou ikiru koto wo nage dasanai de<br>Tsunaida kimi no te wo<br>Itsuka ushinatte shimau no kana<br>Usurete iku egao to kimi wo mamoritai kara<br>Hibiku boku wo yobu koe sae kare<br>Toki ni sou kaze ni kaki kesaretatte<br>Kimi wo mitsuke dasu<br>Ienai itami kanashimi de kizu tsuita kimi<br>Mou waraenai nante hito girai nante kotoba sou iwanai de<br>Mienai mirai ni okoru koto subete ni imi ga aru kara<br>Ima wa sono mama de ii kitto kizukeru toki ga kuru daro<br>Sabikitta hito no you ni<br>Kasanari au dake ga munashikute<br>Hitori de ikite ikerutte itteta<br>Arifureta yasashisa kotobajya<br>Ima wa mou todokanai hodo ni kimi wa uzukidasu<br>Tsunai da kimi no te wa nanigenai yasashisa wo motome<br>Do you remember<br>Itami wo shiru koto de hito ni yasashiku nareru kara<br>Drive your Life<br>Ienai itami kanashimi de kizu tsuita kimi<br>Mou waraenai nante hito girai nante kotoba sou iwanai de<br>Mienai mirai ni okoru koto subete ni imi ga aru kara<br>Ima wa sono mama de ii kitto kizukeru toki ga kuru daro<br>How can I see the meaning of life<br>Kieteku you're the only<br>Kowarenai you ni to hanarete iku kimi<br>Mou waraenai nante hito girai nante kotoba sou iwanai de<br>Ima wa by and by mie nakuttatte subete ni imi ga aru kara<br>Kesenai kako mo seoi attekou ikiru koto wo nagedasanai de<br>You'd better forget everything. Remember. . . your different Life?<br>You'd better forget everything. Remember. . . modoranai kedo<br>Hizunda kioku no you na toki no naka de itsuka wakari aeru kara"
+}
+
+const musica5={
+    nome:"Primeiros Erros",
+    singer:"Capital Inicial",
+    arquivo:"musicas/Capital Inicial - Primeiros Erros.mp3",
+    imagem:"url(img/capital-inicial.jpg)",
+    tempo: "4:15",
+    url: "primeiros-erros.html",
+letra:"Meu caminho é cada manhã<br>Não procure saber onde vou<br>Meu destino não é de ninguém<br>Eu não deixo os meus passos no chão<br>Se você não entende, não vê<br>Se não me vê, não entende<br>Não procure saber onde estou<br>Se o meu jeito te surpreende<br>Se o meu corpo virasse sol<br>Minha mente virasse sol<br>Mas só chove e chove<br>Chove e chove<br>Se um dia eu pudesse ver<br>Meu passado inteiro<br>E fizesse parar de chover<br>Nos primeiros erros, oh<br>O meu corpo viraria sol<br>Minha mente viraria...<br>Mas só chove e chove<br>Chove e chove<br>Se um dia eu pudesse ver<br>Meu passado inteiro<br>E fizesse parar de chover<br>Nos primeiros erros, oh<br>O meu corpo viraria sol<br>Minha mente viraria...<br>Mas só chove e chove<br>Chove e chove, oh<br>O meu corpo viraria sol<br>Minha mente viraria sol<br>Mas só chove e chove<br>Chove e chove, oh<br>Chove e chove, oh<br>Chove e chove, oh<br>Chove e chove"
+}
+
+const musica6={
+    nome:"Pork Soda",
+    singer:"Glass Animals",
+    arquivo:"musicas/Glass Animals - Pork Soda.mp3",
+    imagem:"url(img/pork-soda.webp)",
+    tempo: "4:33",
+    url: "pork-soda.html",
+letra:"Somewhere in South End when you were fun<br>You took my hand and you made me run<br>Up past the prison to the seafront<br>You climbed the cliff edge and took the plunge<br>Why can't we laugh now like we did then?<br>How come I see you and ache instead?<br>How come you only look pleased in bed?<br>Let's climb the cliff edge and jump again<br>Pineapples are in my head<br>(Pineapples are in my head)<br>Got nobody 'cause I'm brain dead<br>(Got nobody 'cause I'm brain dead)<br>Somebody said that I'm a fuckin' slum<br>Don't know that I belong<br>Maybe you're fucking dumb<br>Maybe I'm just a bum<br>Maybe you're fucking scum<br>Don't you go psycho chum<br>I want you for the world<br>I want you all the time<br>Pineapples are in my head<br>(When you were fun)<br>Got nobody 'cause I'm brain dead<br>(You made me run)<br>Pineapples are in my head<br>(To the seafront)<br>Got nobody 'cause I'm brain dead<br>(She took the plunge)<br>5000 footsteps in your wet dress<br>Back to the house with your arms around my neck<br>We drank pork soda with tangled legs<br>I won't forget how you looked at me then<br>I know I'm no sweet prince of love<br>Those times that we got drunk<br>Maybe Jamaica rum<br>Maybe some Johnny Dub<br>Maybe you still think of us<br>Phone buzz, and still I jump<br>Why don't I say it then?<br>I want you all the time<br>Why can't we laugh now like we did then?<br>How come I see you and ache instead?<br>How come you only look pleased in bed?<br>Let's climb the cliff edge and jump again<br>Pineapples are in my head<br>(Pineapples are in my head)<br>Got nobody 'cause I'm brain dead<br>(Got nobody 'cause I'm brain dead)<br>Pineapples are in my head<br>(Pineapples are in my head)<br>Got nobody 'cause I'm brain dead<br>(Got nobody 'cause I'm brain dead)<br>Pineapples are in my head<br>(Pineapples are in my head)<br>Got nobody 'cause I'm brain dead<br>(Got nobody 'cause I'm brain dead)<br>Pineapples are in my head<br>(Pineapples are in my head)<br>Got nobody 'cause I'm brain dead<br>(Got nobody 'cause I'm brain dead)"
+}
+
+const musica7={
+    nome:"Levanta e anda",
+    singer:"Emicida Feat. Rael",
+    arquivo:"musicas/Emicida - Levanta e anda (Feat. Rael).mp3",
+    imagem: "url(img/levantaeanda.jpg)",
+    tempo: "2:49",
+    url: "levanta-e-anda.html",
+    letra: "Era um cômodo incômodo<br>Sujo como o dragão de komodo<br> Úmido, eu, homem da casa<br> Aos seis anos<br> Mofo no canto, todo TV<br> Engodo pronto pro lodo<br>Tímido, porra!<br>Somos reis, mano<br>Olhos são elétrodos, sério<br>Topo, trombo corvos<br>Num cemitério de sonhos<br>Graças a leis, planos<br>Troco de jogo vendo, roubo<br>Pus a cabeça a prêmio, ingênuo<br>Colhi sorrisos e falei vamos<br>É um novo tempo, momento<br>Pro novo, ao sabor do vento<br>Eu me movo pelo solo onde reinamos<br>Pondo pontos finais na dor, como<br>Doril, Anador e somos a luz do senhor<br>E pode crê, tamo construindo<br>Suponho não, creio, meto a mão<br>Em meio à escuridão pronto aceitamos<br>Nosso sorriso sereno hoje é o veneno<br>Pra quem trouxe tanto ódio pra<br>Onde deitamos<br>Quem costuma vir de onde eu sou<br>Às vezes não tem motivos pra seguir!<br>Então levanta e anda, vai, levanta e anda<br> Vai, levanta e anda<br> Mas eu sei que vai, que o sonho te traz<br> Coisas que te faz prosseguir!<br>Vai, levanta e anda, vai, levanta e anda<br>Vai, levanta e anda, vai, levanta e anda<br>Irmão, você não percebeu<br>Que você é o único representante<br>Do seu sonho na face da terra<br>Se isso não fizer você correr, chapa<br>Eu não sei o que vai<br>Eu sei (sei!), cansa<br>Quem morre ao fim do mês<br> Nossa grana ou nossa esperança<br>Delírio é, equilíbrio<br>Entre nosso martírio e nossa fé<br>Foi foda contar migalha nos escombro<br>Lona preta esticadas, enxada no ombro<br> E nada vim, nada enfim<br>Recria sozinho<br>Com a alma cheia de mágoa e as panela vazia<br>Sonho imundo, só água na geladeira<br>E eu querendo salvar o mundo<br>No fundo é tipo David Blaine<br>A mãe assume, o pai some de costume<br>No máximo, é um sobrenome<br>Sou o terror dos clone<br>Esses boy conhece Marx<br>Nós conhece a fome<br>Então cerra os punho, sorria<br> E jamais volte pra sua quebrada de mão e mente vazia<br>Quem costuma vir de onde eu sou<br>Às vezes não tem motivos pra seguir<br>Então levanta e anda, vai, levanta e anda<br>Vai, levanta e anda<br>Mas eu sei que vai, que o sonho te traz<br>Coisas que te faz prosseguir<br>Então levanta e anda, vai, levanta e anda<br> Vai, levanta e anda, vai, levanta e anda<br>Somos maior, nos basta só sonhar, seguir"
+}
+
+
+const musicas=[musica1, musica2, musica3, musica4, musica5, musica6, musica7];
+let nomeMusica=document.querySelector(".music-name");
+let singerMusica=document.querySelector(".singer-name");
+let letraMusica=document.querySelector(".letra-song");
+let imagemMusica=document.querySelector(".box-img-musica");
+let musica=document.querySelector("audio");
+let play=document.querySelector(".btn-play");
+let pause=document.querySelector(".btn-pause");
+let previous=document.querySelector(".btn-previous");
+let next=document.querySelector(".btn-next");
+let volume=document.querySelector(".btn-volume");
+let mute=document.querySelector(".btn-muted");
+let barra=document.querySelector("progress");
+var i=0;
+
+
+    for(var j=0; j<musicas.length; j++){
+        document.querySelector(".conteudo").innerHTML+="<div class='box-musica' id='"+j+"'><div class='img-musica'></div><label><h3 class='music-name'>"+musicas[j].nome+"</h3><p class='singer-name'>"+musicas[j].singer+"</p></label><p id='music-time'>"+musicas[j].tempo+"</p><div class='corazon'></div><div class='plus'></div> </div>"
+    }
+
+    for(let i=0; i<musicas.length; i++){
+        document.querySelectorAll(".img-musica")[i].style.backgroundImage=musicas[i].imagem;
+    }
+    
+    for(let i=0; i<musicas.length; i++){
+    document.querySelectorAll(".box-musica")[i].addEventListener("click", function(){
+         renderizarMusica(i)
+    })
+}
+
+
+play.addEventListener("click", function playMusica(){
+    musica.play();
+    play.style.display="none";
+    pause.style.display="block";
+})
+
+pause.addEventListener("click", function pauseMusica(){
+    musica.pause();
+    play.style.display="block";
+    pause.style.display="none";
+})
+
+previous.addEventListener("click", function previousMusica(){
+    play.style.display="block";
+    pause.style.display="none";
+    barra.style.width=0;
+    i--;
+    if(i<0){
+        i=musicas.length-1;
+    }
+    renderizarMusica(i);
+})
+
+next.addEventListener("click", function nextMusica(){
+    play.style.display="block";
+    pause.style.display="none";
+    barra.style.width=0;
+    i++;
+    if(i>musicas.length-1){
+        i=0;
+    }
+    renderizarMusica(i);
+    
+})
+
+volume.addEventListener("click", function mutar(){
+    musica.muted=true;
+    volume.style.display="none";
+    mute.style.display="block";
+})
+
+mute.addEventListener("click", function desmutar(){
+    musica.muted=false;
+    volume.style.display="block";
+    mute.style.display="none";
+})
+
+musica.addEventListener("timeupdate", function atualizarBarra(){
+    barra.style.width=(musica.currentTime/musica.duration)*100+"%";
+})
+
+
+function renderizarMusica(index){
+    document.querySelector("body").style.width="100vw";
+    document.querySelector("body").style.height="100vh";
+    document.querySelector(".main-home").style.display="none";
+    document.querySelector(".main-musica").style.display="flex";
+    i=index;
+    document.querySelector("title").innerHTML=musicas[index].nome+" - "+musicas[index].singer;
+    nomeMusica.innerHTML=musicas[index].nome;
+    singerMusica.innerHTML=musicas[index].singer;
+    musica.setAttribute('src', musicas[index].arquivo);
+    imagemMusica.style.backgroundImage=musicas[index].imagem;
+    letraMusica.innerHTML=musicas[index].letra;
+}
+
+function paginaInicial(){
+    document.querySelector("title").innerHTML="CatSong - Home";
+    play.style.display="block";
+    pause.style.display="none";
+    barra.style.width=0;
+    musica.pause();
+    document.querySelector("body").style.width="100%";
+    document.querySelector("body").style.height="100%";
+    document.querySelector(".main-home").style.display="block";
+    document.querySelector(".main-musica").style.display="none";
+    selecionarMusica(i);
+}
+
+    function selecionarMusica(index){
+        for(let i=0; i<musicas.length; i++){
+            document.querySelectorAll(".box-musica")[i].classList.remove("marcado")
+        
+    }
+    document.querySelectorAll(".box-musica")[index].classList.add("marcado")
+}
